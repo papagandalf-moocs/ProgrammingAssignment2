c5b08b1f08f20de2f5b1b09af49c0697c349a3e1
papagandalf <papagandalf@localhost.localdomain>
2014-09-21 22:59:35 +0300

    exercise solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
+## implementation of the "special" matrix, whose inverse we can cache
 ## functions do
 
-## Write a short comment describing this function
+## creates the "special" matrix in a way similar to the one given in the vector example
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(inverse) m <<- inverse
+        getInverse <- function() m
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## returns the inverse of the matrix, after checking if it has been calculated and cached before
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+        m <- x$getInverse()
+        if(!is.null(m))
+                return(m)
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setInverse(m)
+        m
 }
+
